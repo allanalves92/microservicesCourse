f987c5c6ba4b89a5185332c791b41727b0fbc089
Allan Alves <allanalvesmail@gmail.com>
2021-06-10 15:07:30 -0300

    Rest controller for withdrawing funds

--- bank-account/bankacc.cmd.api/src/main/java/com/springbank/bankacc/cmd/api/controllers/OpenAccountController.java
+++ bank-account/bankacc.cmd.api/src/main/java/com/springbank/bankacc/cmd/api/controllers/OpenAccountController.java
@@ -1,23 +1,20 @@
 package com.springbank.bankacc.cmd.api.controllers;
 
-import com.springbank.bankacc.cmd.api.commands.OpenAccountCommand;
-import com.springbank.bankacc.cmd.api.dto.OpenAccountResponse;
-import org.axonframework.commandhandling.gateway.CommandGateway;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.springbank.bankacc.cmd.api.commands.*;
+import com.springbank.bankacc.cmd.api.dto.*;
+import org.axonframework.commandhandling.gateway.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.*;
+import org.springframework.security.access.prepost.*;
+import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import java.util.UUID;
+import javax.validation.*;
+import java.util.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/openBankAccount")
 public class OpenAccountController {
+
     private final CommandGateway commandGateway;
 
     @Autowired
@@ -34,12 +31,15 @@ public class OpenAccountController {
         try {
             commandGateway.send(command);
 
-            return new ResponseEntity<>(new OpenAccountResponse(id, "successfully opened a new bank account!"), HttpStatus.CREATED);
+            return new ResponseEntity<>(new OpenAccountResponse(id, "successfully opened a new " +
+                    "bank account!"), HttpStatus.CREATED);
         } catch (Exception e) {
-            var safeErrorMessage = "Error while processing request to open a new bank account for id - " + id;
+            var safeErrorMessage = "Error while processing request to open a new bank account for" +
+                    " id - " + id;
             System.out.println(e.toString());
 
-            return new ResponseEntity<>(new OpenAccountResponse(id, safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new OpenAccountResponse(id, safeErrorMessage),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
